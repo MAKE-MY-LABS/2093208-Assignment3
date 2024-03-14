ab86ed60148da24c300030601bebd0ae6b9a2d33
Nikhil <nikhil.pulluri@cognizant.com>
2024-03-14 11:14:50 +0530

    add cors

--- Assgiment3/cryptocurrency/src/main/java/com/cog/cryptocurrency/CryptocurrencyApplication.java
+++ Assgiment3/cryptocurrency/src/main/java/com/cog/cryptocurrency/CryptocurrencyApplication.java
@@ -2,12 +2,27 @@ package com.cog.cryptocurrency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CryptocurrencyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptocurrencyApplication.class, args);
+	}
+
+	// add bean config for allow all origins
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// append all methods to the registry
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
